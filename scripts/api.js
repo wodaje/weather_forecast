127ef120fb84ba9f349f1d3d594bbbdea6e3a8a3
Jeff Woda <wodahouse@gmail.com>
2020-12-08 17:58:11 -0500

    First section API functionality

--- scripts/api.js
+++ scripts/api.js
@@ -14,19 +14,80 @@
 
 // http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}  uv index = requrires lat and lon
 
+// woda api key access free account
 var apiKey = "6baec66594d536d831469149c42d3d07"
 
+/// Current Weather Var for Object
+var cW = Object
+// 5day Weather Var
+var dW = Object
+// UvIndex Var
+var uvI = Object
+
+
+
 //function to make API Call
-async function apiCall(urlCall){
+async function apiCallcW(urlCall){
 
-let response = await $.ajax({url: urlCall, method: "GET"});
+let r = await $.ajax({url: urlCall, method: "GET"});
 
-console.log(response)
+console.log(r)
+cW= (r)
+lat = r.coord.lat
+lon = r.coord.lon
 
-$(".container").append(response)
+let r2 = await $.ajax({url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`, method: "GET"});
+console.log(r2)
+uvI = (r2)
+
+//populateScreen()
 
 }
 
-apiCall(`https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=${apiKey}`)
+async function apiCalluV(urlCall){
+    
+    console.log(lat)
+    console.log(lon)
 
- 
+
+    let response = await $.ajax({url: urlCall, method: "GET"});
+    
+    console.log(response)
+    uvI= (response)
+      
+    
+    populateScreen()
+    
+    }
+
+
+apiCallcW(`https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=${apiKey}`)
+//apiCalluV(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+
+
+function populateScreen(){
+
+//let br = document.createElement("br")
+
+$(".container").append("Name: " + cW.name).append('<br />')
+
+$(".container").append("Humidity: " + cW.main.humidity).append('<br />')
+
+$(".container").append("Temperature: " + cW.main.temp).append('<br />')
+
+ // Creating an element to hold the image
+ var image = $("<img>").attr("src", `http://openweathermap.org/img/wn/${cW.weather[0].icon}@2x.png`);
+ $(".container").append(image).append('<br />')
+
+//console.log(iconPic)
+
+$(".container").append("Wind Speed: " + cW.wind.speed).append('<br />')
+
+$(".container").append("Date?: " + cW.dt_iso).append('<br />')
+    
+}
+
+
+
+
+
