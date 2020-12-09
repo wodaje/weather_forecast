22433694e007edb4d73b3c3544998fd254e2245c
Jeff Woda <wodahouse@gmail.com>
2020-12-09 17:47:29 -0500

    finished API call functionality

--- scripts/api.js
+++ scripts/api.js
@@ -1,20 +1,4 @@
-// Current Weather Data 
-
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-// api key for above 6baec66594d536d831469149c42d3d07
-
-//q = city name  units = imperial
-
-//*********************************************
-
-// Call 5 day / 3 hour forecast data 
-
-//api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
-
-// http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}  uv index = requrires lat and lon
-
-// woda api key access free account
+// API key access free account
 var apiKey = "6baec66594d536d831469149c42d3d07"
 
 /// Current Weather Var for Object
@@ -24,51 +8,42 @@ var dW = Object
 // UvIndex Var
 var uvI = Object
 
-
+apiCall(`https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=${apiKey}`)
 
 //function to make API Call
-async function apiCallcW(urlCall){
+async function apiCall(urlCall){
 
-let r = await $.ajax({url: urlCall, method: "GET"});
+    try {
+        const r = await $.ajax({url: urlCall, method: "GET"});
+        // populate var for function ref
+        cW= (r)
+        // pull lattitude and longitude for following API calls
+        let lat = r.coord.lat
+        let lon = r.coord.lon
 
-console.log(r)
-cW= (r)
-lat = r.coord.lat
-lon = r.coord.lon
+    if (r) {
+        const r2 = await $.ajax({url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`, method: "GET"});
+        // populate var for function ref
+        uvI = (r2)
 
-let r2 = await $.ajax({url: `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`, method: "GET"});
-console.log(r2)
-uvI = (r2)
-
-//populateScreen()
+        const r3 = await $.ajax({url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`, method: "GET"});
+        // populate var for function ref
+        dW = (r3)
+    }
+    }catch(error){
+        console.log(error)
+    }
+  
+    populateScreen()   
 
 }
-
-async function apiCalluV(urlCall){
-    
-    console.log(lat)
-    console.log(lon)
-
-
-    let response = await $.ajax({url: urlCall, method: "GET"});
-    
-    console.log(response)
-    uvI= (response)
-      
-    
-    populateScreen()
-    
-    }
-
-
-apiCallcW(`https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&appid=${apiKey}`)
-//apiCalluV(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`)
 
 
 function populateScreen(){
 
 //let br = document.createElement("br")
 
+$(".container").append('<br />')
 $(".container").append("Name: " + cW.name).append('<br />')
 
 $(".container").append("Humidity: " + cW.main.humidity).append('<br />')
@@ -83,11 +58,39 @@ $(".container").append("Temperature: " + cW.main.temp).append('<br />')
 
 $(".container").append("Wind Speed: " + cW.wind.speed).append('<br />')
 
-$(".container").append("Date?: " + cW.dt_iso).append('<br />')
+let dateEl = cW.dt
+console.log(dateEl)
+dateEl = moment.unix(dateEl).format('(MM/DD/YYYY)')
+
+$(".container").append("Date: " + dateEl).append('<br />')
     
 }
 
 
+
+//  ***NOTES***
+
+//let dateConvEl = (dW.daily[0].dt) 
+//dateConvEl = moment.unix(dateConvEl).format('(MM/DD/YYYY)')
+
+
+
+
+// conversion moment.unix(yourUnixEpochTime).format('dddd, MMMM Do, YYYY h:mm:ss A')
+
+
+//async function getData() {
+//  try {
+//    const firstCall = await ajax()
+
+// check for data from first call
+//    if (firstCall) {
+//      const secondCall = await ajax(`/url/${firstCall}`)
+//    }
+//  } catch (err) {
+//    console.log(err)
+//  }
+//}  
 
 
 
